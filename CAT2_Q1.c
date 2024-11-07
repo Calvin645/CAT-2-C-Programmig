904509177edd26fa9d1b0e8322887b66d4f589a4
Calvin645 <calvinpaul913@gmail.com>
2024-11-07 16:59:10 +0300

    Update CAT2_Q1.c

--- CAT2_Q1.c
+++ CAT2_Q1.c
@@ -1,4 +1,8 @@
+// Online C compiler to run C program online
 #include <stdio.h>
+#include <string.h>
+
+// defines members of the structure
 
 struct employee {
     char name[25];
@@ -6,24 +10,22 @@ struct employee {
     char department[20];
     float salary;
     char email[50];
-};
+}; struct employee em1;
 
 int main() {
-    // employee variable
-    struct employee emp = {
-        "John Doe",
-        12345,
-        "Human Resources",
-        55000.50,
-        "john.doe@company.com"
-    };
+    strcpy(em1.name, "John Doe");
+     em1.id = 12345;
+    strcpy(em1.department, "Human Resources");
+     em1.salary = 55000.50;
+    strcpy(em1.email, "john.doe2company.com");
+    
 
     // field values
-    printf("Name: %s\n", emp.name);
-    printf("ID: %d\n", emp.id);
-    printf("Department: %s\n", emp.department);
-    printf("Salary: %.2f\n", emp.salary);
-    printf("Email: %s\n", emp.email);
+    printf("Name: %s\n", em1.name);
+    printf("ID: %d\n", em1.id);
+    printf("Department: %s\n", em1.department);
+    printf("Salary: %.2f\n", em1.salary);
+    printf("Email: %s\n", em1.email);
 
     return 0;
 }
