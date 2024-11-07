8639a4f60ec39b6c885e04a84abbe31b9782608f
Calvin645 <calvinpaul913@gmail.com>
2024-11-08 00:09:45 +0300

    Update CAT2_Q2.c

--- CAT2_Q2.c
+++ CAT2_Q2.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    // 2D array
-    int scores[2][4] = {
-        {65,92,24,72},
-        {35,70,59,67}
-    };
+    //  2 2D array
+    int scores[2][2][2] = {{{65,92},{84,72}},{{35,70},{59,67}}};
+    int i,j,k;
 
   
     for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 4; j++) {
-            printf("%d ", scores[i][j]);
+        for (int j = 0; j < 2; j++) {
+            for(k=0; k<2; k++) {
+                
+            printf("scores[%d][%d][%d] %d\n", scores[i][j][k]);
         }
-        printf("\n"); 
+        }
     }
 
     return 0;
